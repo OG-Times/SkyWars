c965048fa23a0ab475a897239fcfc988b48d41b1
jsexp <108526035+hardcorefactions@users.noreply.github.com>
2026-02-20 15:54:27 +0100

    Refactor scoredSidebar method for clarity and efficiency

--- spigot/src/main/java/fun/ogtimes/skywars/spigot/utils/BoardAPI.java
+++ spigot/src/main/java/fun/ogtimes/skywars/spigot/utils/BoardAPI.java
@@ -13,80 +13,71 @@ public class BoardAPI {
         if (var67 == null) {
             var67 = "Unamed board";
         }
-
         if (var67.length() > 32) {
             var67 = var67.substring(0, 32);
         }
-
         String var4;
         label39:
-        for(; var2.size() > 16; var2.remove(var4)) {
-            var4 = (String)var2.keySet().toArray()[0];
+        for (; var2.size() > 16; var2.remove(var4)) {
+            var4 = (String) var2.keySet().toArray()[0];
             int var5 = var2.get(var4);
             Iterator<String> var6 = var2.keySet().iterator();
-
-            while(true) {
+            while (true) {
                 String var7;
                 do {
-                    if (!var6.hasNext()) {
-                        continue label39;
-                    }
-
+                    if (!var6.hasNext()) continue label39;
                     var7 = var6.next();
-                } while(var2.get(var7) >= var5 && (var2.get(var7) != var5 || var7.compareTo(var4) >= 0));
-
+                } while (var2.get(var7) >= var5 && (var2.get(var7) != var5 || var7.compareTo(var4) >= 0));
                 var4 = var7;
                 var5 = var2.get(var7);
             }
         }
 
-        String finalVar6 = var67;
+        String finalTitle = var67;
         Bukkit.getScheduler().runTask(SkyWars.getPlugin(), () -> {
-            if (var0 != null && var0.isOnline()) {
-                if (Bukkit.getScoreboardManager().getMainScoreboard() != null && Bukkit.getScoreboardManager().getMainScoreboard() == var0.getScoreboard()) {
-                    var0.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
-                }
+            if (var0 == null || !var0.isOnline()) return;
 
-                if (var0.getScoreboard() == null) {
-                    var0.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
-                }
-
-                Bukkit.getScheduler().runTaskAsynchronously(SkyWars.getPlugin(), () -> {
-                    Objective var3 = var0.getScoreboard().getObjective(DisplaySlot.SIDEBAR);
-                    if (var3 == null) {
-                        var3 = var0.getScoreboard().registerNewObjective(finalVar6.length() > 16 ? finalVar6.substring(0, 15) : finalVar6, "dummy");
-                    }
-
-                    var3.setDisplayName(finalVar6);
-                    if (var3.getDisplaySlot() == null || var3.getDisplaySlot() != DisplaySlot.SIDEBAR) {
-                        var3.setDisplaySlot(DisplaySlot.SIDEBAR);
-                    }
-
-                    Iterator<String> var728572 = var2.keySet().iterator();
-
-                    while(true) {
-                        String var5;
-                        do {
-                            if (!var728572.hasNext()) {
-                                var728572 = var0.getScoreboard().getEntries().iterator();
-
-                                while(var728572.hasNext()) {
-                                    var5 = var728572.next();
-                                    if (var3.getScore(var5).isScoreSet() && !var2.containsKey(var5)) {
-                                        var0.getScoreboard().resetScores(var5);
-                                    }
-                                }
-
-                                return;
-                            }
-
-                            var5 = var728572.next();
-                        } while(var3.getScore(var5).isScoreSet() && var3.getScore(var5).getScore() == var2.get(var5));
-
-                        var3.getScore(var5).setScore(var2.get(var5));
-                    }
-                });
+            if (var0.getScoreboard() == null ||
+                    var0.getScoreboard() == Bukkit.getScoreboardManager().getMainScoreboard()) {
+                var0.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
             }
+
+            Objective obj = var0.getScoreboard().getObjective(DisplaySlot.SIDEBAR);
+            if (obj == null) {
+                obj = var0.getScoreboard().registerNewObjective(
+                        finalTitle.length() > 16 ? finalTitle.substring(0, 15) : finalTitle,
+                        "dummy"
+                );
+            }
+            obj.setDisplayName(finalTitle);
+            if (obj.getDisplaySlot() != DisplaySlot.SIDEBAR) {
+                obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+            }
+
+            final Objective finalObj = obj;
+            Bukkit.getScheduler().runTaskAsynchronously(SkyWars.getPlugin(), () -> {
+                Iterator<String> it = var2.keySet().iterator();
+                while (true) {
+                    String entry;
+                    do {
+                        if (!it.hasNext()) {
+                            // Clean up removed entries
+                            Iterator<String> cleanup = var0.getScoreboard().getEntries().iterator();
+                            while (cleanup.hasNext()) {
+                                String e = cleanup.next();
+                                if (finalObj.getScore(e).isScoreSet() && !var2.containsKey(e)) {
+                                    var0.getScoreboard().resetScores(e);
+                                }
+                            }
+                            return;
+                        }
+                        entry = it.next();
+                    } while (finalObj.getScore(entry).isScoreSet() &&
+                             finalObj.getScore(entry).getScore() == var2.get(entry));
+
+                    finalObj.getScore(entry).setScore(var2.get(entry));
+                }
+            });
         });
     }
 }
